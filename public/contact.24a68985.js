669cacbf10bf8c9ad6f8e648721d09bb92931fe1
Marvelmta <getrektbymarcel@gmail.com>
2025-04-01 14:05:46 +0200

    commit

--- public/contact.24a68985.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit",function(e){e.preventDefault();let t=document.getElementById("confirmationMessage");t.style.display="block",this.reset(),setTimeout(()=>{t.style.display="none"},5e3)});
-//# sourceMappingURL=contact.24a68985.js.map
